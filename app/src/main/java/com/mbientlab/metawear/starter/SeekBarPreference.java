4aea375012d47747c4b61025f5adc21723c07838
Arto Heino <artohei@Artos-MacBook-Pro.local>
2016-10-10 13:53:15 +0300

    Add sliders to setting alert values

---
+++ app/src/main/java/com/mbientlab/metawear/starter/SeekBarPreference.java
@@ -0,0 +1,87 @@
+package com.mbientlab.metawear.starter;
+
+/**
+ * Created by artohei on 10/10/16.
+ */
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class SeekBarPreference extends Preference implements SeekBar.OnSeekBarChangeListener {
+    private SeekBar mSeekBar;
+    private int mProgress;
+    private TextView mProgressDisplay;
+
+    public SeekBarPreference(Context context) {
+
+        this(context, null, 0);
+    }
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+
+        this(context, attrs, 0);
+    }
+
+    public SeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setLayoutResource(R.layout.preference_seekbar);
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        mSeekBar = (SeekBar) view.findViewById(R.id.seekbar);
+        mProgressDisplay = (TextView) view.findViewById(R.id.summary);
+        mSeekBar.setMax(60);
+        mSeekBar.setProgress(mProgress);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mProgressDisplay.setText(String.valueOf(mProgress));
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (!fromUser)
+            return;
+
+        setValue(progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // not used
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // not used
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        setValue(restoreValue ? getPersistedInt(mProgress) : (Integer) defaultValue);
+    }
+
+    public void setValue(int value) {
+        if (shouldPersist()) {
+            persistInt(value);
+        }
+
+        if (value != mProgress) {
+            mProgress = value;
+            notifyChanged();
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+
+        return a.getInt(index, 0);
+    }
+}
